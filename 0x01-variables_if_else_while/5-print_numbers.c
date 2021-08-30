2d711aa36c272b57cc4b6d481a413aeebd38ad6e
akliluwoldehana@gmail.com <Akliluwoldehana@gmail.com>
2021-08-30 08:54:22 -0400

    Base 10 nums

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	signed char x;
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+	printf("%d", n);
+	if (n == 10)
+	{
+	printf("\n");
+	}
+
+	}
+	return (0);
+}
