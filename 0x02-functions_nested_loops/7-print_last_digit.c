9d63aa670fe5c0807b7f97b4c062b4c07b931bbd
akliluwoldehana@gmail.com <Akliluwoldehana@gmail.com>
2021-08-31 14:38:25 -0400

    a function to get last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit  - find absolute value
+ * @n: integer to hold number
+ * Return: abs value of n
+ */
+int print _last_digit(int n)
+{
+	return (n % 10);
+}
