37b672af7ede28952dec7c76ee6e905b1fa17de7
akliluwoldehana@gmail.com <Akliluwoldehana@gmail.com>
2021-08-31 15:27:55 -0400

    learn your time table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,8 +8,9 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int n)
+int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
+void print_to_98(int n);
 
 #endif
