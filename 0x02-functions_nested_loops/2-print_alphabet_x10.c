ba860c3e07e3ab000e3f417f0cff20531f23b1e8
akliluwoldehana@gmail.com <Akliluwoldehana@gmail.com>
2021-08-31 13:51:00 -0400

    a function to print lowercase 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ * print_alphabet_x10(void) prints the alphabet, in lowercase 10x followed by a new line
  */
 void print_alphabet_x10(void)
 {
@@ -15,6 +15,5 @@ void print_alphabet_x10(void)
 	}
 
 	_putchar('\n');
-	
 	}
 }
