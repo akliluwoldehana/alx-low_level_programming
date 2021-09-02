7c83595cb1234b8854fa1feff74123505b89d589
akliluwoldehana@gmail.com <Akliluwoldehana@gmail.com>
2021-09-02 12:35:53 -0400

    Largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,26 +9,20 @@
  */
 int largest_number(int a, int b, int c)
 {
-	int largest;
+    int largest;
 
-	if (a > b)
-	{
-	if (a > c)
-	{
-		largest = a;
-	}
-	else
-	{
-		largest = c;
-	}
-	}
-	else if (b > c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
-	return (largest);
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else if (c > a && c > b)
+    {
+        largest = c;
+    }
+
+    return (largest);
 }
