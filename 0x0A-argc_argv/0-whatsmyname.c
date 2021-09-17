aba69af3a035f186bd9f148d6ebb17e7d0158f21
HP <akliluwoldehana@gmail.com>
2021-09-17 15:55:15 +0300

    C arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - prints programs name
+ * @argc : argument count
+ * @argv : argument content
+ * Return : 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+
+	printf("%s",argv[0]);
+	return (0);
+}
+
